6112ded1c4df58f0e0a0fc978484b8b82619cc0f
M. Knyazev <mvknyazev@alfabank.ru>
2021-01-21 20:59:07 +0300

    feature: tiles color changed & tilemaps collapsed into one

---
+++ Assets/Scripts/SnakeMovingSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovingSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("MoveSnakeForward", 1.0f, 1.0f);
+    }
+
+
+    void MoveSnakeForward()
+    {
+        Debug.Log("Hello");
+    }
+}

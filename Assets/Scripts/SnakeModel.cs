bfda9e8731a0b6ea84e95e89d4e0d613c5802b07
M. Knyazev <mvknyazev@alfabank.ru>
2021-01-21 22:02:35 +0300

    feature: script draft [wip]

---
+++ Assets/Scripts/SnakeModel.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class SnakeModel
+{
+
+    public Vector2 FacingDirection { get; set; }
+    private List<Vector2> _positions;
+
+    public SnakeModel(Vector2 facingDirection, ICollection<Vector2> initialCoordinates)
+    {
+        _positions = new List<Vector2>(initialCoordinates);
+        this.FacingDirection = facingDirection;
+    }
+
+    public List<Vector2> MoveForward()
+    {
+        Vector2[] result = new Vector2[_positions.Count];
+        Vector2 prevHeadPos = Vector2.zero;
+        for (int i = 0; i < _positions.Count; i++)
+        {
+            
+            if (i == 0)
+            {
+                prevHeadPos = _positions[i];
+                result[i] = _positions[i] + FacingDirection;
+                continue;
+            }
+            if (_positions.Count > 1)
+            {
+                if (i != _positions.Count - 1)
+                {
+                    result[i + 1] = _positions[i];
+                }
+                else
+                {
+                    
+                    result[1] = prevHeadPos;
+                    
+                }
+            }
+            
+        }
+
+        _positions = new List<Vector2>(result);
+        return new List<Vector2>(result);
+    }
+}
--- Assets/Scripts/SnakeMovingSystem.cs
+++ Assets/Scripts/SnakeMovingSystem.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class SnakeMovingSystem : MonoBehaviour
 {
+
+    public Tilemap tilemap;
+    public Tile snakeHeadTile;
+    public Tile snakeBodyTile;
+
+    public SnakeModel snakeModel;
+
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("MoveSnakeForward", 1.0f, 1.0f);
+
+        LinkedList<Vector2> snakePositions = new LinkedList<Vector2>();
+        for (int y = tilemap.origin.y; y < (tilemap.origin.y + tilemap.size.y); y++)
+        {
+            for (int x = tilemap.origin.x; x < (tilemap.origin.x + tilemap.size.x); x++)
+            {
+                Tile tile = tilemap.GetTile<Tile>(new Vector3Int(x, y, 0));
+                if (tile != null )
+                {
+                    // fixme: this part does not work
+                    if (tile.GetType() == snakeBodyTile.GetType())
+                    {
+                        snakePositions.AddLast(new Vector2(x, y));
+                    }
+                    if (tile.GetType() == snakeHeadTile.GetType())
+                    {
+                        snakePositions.AddFirst(new Vector2(x, y));
+                    }
+                }
+            }
+        }
+        
+
+        snakeModel = new SnakeModel(Vector2.up, snakePositions);
     }
 
 
     void MoveSnakeForward()
     {
-        Debug.Log("Hello");
+        Debug.Log(snakeModel.MoveForward().ToString());
     }
 }
